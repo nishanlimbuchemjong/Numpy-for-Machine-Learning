9f0304f3af906f720895b199b56d53186c232a91
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-04 18:21:29 +0545

    found sum, mean, and standard deviation on sum_mean_stand_dev_question.py file under introduction directory

---
+++ Introduction to NumPy/sum_mean_stand_dev_question.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+np1 = np.array([[3, 4, 5], [1, 2, 3], [5, 6, 7]])
+
+print(np1)
+
+sum_result = np.sum(np1)
+print(f"Sum = {sum_result}")
+
+mean_result = np.mean(np1)
+print(f"Mean = {mean_result}")
+
+st_deviation_result = np.std(np1)
+print(f"Standard deviation = {st_deviation_result}")
