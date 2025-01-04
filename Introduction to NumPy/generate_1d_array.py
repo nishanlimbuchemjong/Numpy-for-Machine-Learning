6d97bde160a05ea48f57edbee5c7eeaa81fcf9ff
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-04 18:29:33 +0545

    generated 1d array of 10 evenly spaced numbers between 0 and 1 on generate_1d_array.py file

---
+++ Introduction to NumPy/generate_1d_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# generating 1-D array of 10 evenly spaced numbers between 0 and 1
+np1 = np.linspace(0, 1, 10)
+print(np1) 
+# output: 
+    # [0.         0.11111111 0.22222222 0.33333333 0.44444444 0.55555556
+    #  0.66666667 0.77777778 0.88888889 1.        ]
--- Introduction to NumPy/sum_mean_stand_dev_question.py
+++ Introduction to NumPy/sum_mean_stand_dev_question.py
@@ -5,10 +5,10 @@ np1 = np.array([[3, 4, 5], [1, 2, 3], [5, 6, 7]])
 print(np1)
 
 sum_result = np.sum(np1)
-print(f"Sum = {sum_result}")
+print(f"Sum = {sum_result}")    # output: Sum = 36
 
-mean_result = np.mean(np1)
-print(f"Mean = {mean_result}")
+mean_result = np.mean(np1)  
+print(f"Mean = {mean_result}")  # # output: Mean = 4.0      
 
 st_deviation_result = np.std(np1)
-print(f"Standard deviation = {st_deviation_result}")
+print(f"Standard deviation = {st_deviation_result}") # output: Standard deviation = 1.8257418583505538
