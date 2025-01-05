2c16ada27d607360c5250d317d81cfad94710fe3
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 18:42:20 +0545

    cross product of two  vector calculated on cross_product.py file

---
+++ Mathematics/Linear Algebra/Vectors/cross_product.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# initializing the two vectors
+vector_a = np.array([1, 2, 3])
+vector_b = np.array([4, 5, 6])
+
+# calculating cross product
+result = np.cross(vector_a, vector_b)
+
+# printing the result
+print(result)   # output: [-3  6 -3]
