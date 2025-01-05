9566d39b5c24a8f8ce46ff0fb1fc0b2d992ede3c
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 17:48:24 +0545

    Min and Max found on statistics_practice.py file under 'Statistics' folder

---
+++ Mathematics/Statistics/statistics_practice.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+stats = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(stats)
+# output:
+#     [[1 2 3]
+#     [4 5 6]]
+
+# finding minimum value
+print(np.min(stats))    # output: 1
+
+# finding maximum value
+print(np.max(stats))    # output: 6
