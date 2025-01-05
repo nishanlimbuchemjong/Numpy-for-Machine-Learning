474132b68a8a1e11863b4a3c37b923dc1a04f8ab
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 19:25:49 +0545

    Trace of matrix calculated on trace_matrix.py file

---
+++ Mathematics/Linear Algebra/Matrix/trace_matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# initializing the matrix
+matrix_a = np.array([[1, 2, 5], [4, 4, 6], [2, 8, 1]])
+
+# calculating the trace of matrix
+result = np.trace(matrix_a)
+
+print(result)   # output: 6
