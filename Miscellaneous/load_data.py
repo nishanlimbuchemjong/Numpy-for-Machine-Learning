5e409ba787be419d28fef8d35ac065e2ab16c744
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 18:13:47 +0545

    boolean masking and advanced indexing on load_data.py file

--- Miscellaneous/load_data.py
+++ Miscellaneous/load_data.py
@@ -21,3 +21,23 @@ print(datas.astype('int32'))
  [  1  22  33  11 999  11   2   1  78   0   1   2   9   8   7   1  76  88]]
 
 """
+
+# Boolean Masking and Advanced Indexing
+
+# finding if the elements are greater than 50
+print(datas>50)
+"""
+[[False False False False  True  True False False False False False False
+  False False False False False False]
+ [False False False False  True  True False  True False False False False
+  False False False False False False]
+ [False False False False  True False False False  True False False False
+  False False False False  True  True]]
+
+"""
+
+# finding the all elements that is greater than 50
+print(datas[datas > 50])    # output: [196.  75. 766.  75.  55. 999.  78.  76.  88.]
+
+# finding all the elements that is greater than 50 and less than 100
+print(datas[(datas > 50) & (datas < 100)])  # output: [75. 75. 55. 78. 76. 88.]
