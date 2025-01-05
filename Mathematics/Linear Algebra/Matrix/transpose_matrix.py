5c3ba7d6c2a23c6f6ec6fed883683b455673d92c
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 19:07:55 +0545

    Transpose of a matrix found on transpose_matrix.py file

---
+++ Mathematics/Linear Algebra/Matrix/transpose_matrix.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# initializing the matrix
+matrix_a = np.array([[1, 2, 3], [4, 5, 6]])
+
+# calculating the transpose of a matrix
+result = np.transpose(matrix_a)
+
+print(result)
+# output:
+#     [1 4]
+#     [2 5]
+#     [3 6]]
