58b65479ea31201011f2c1a71b9a1a7c352bef3f
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 17:36:23 +0545

    different arithmetic operations practiced on arithmetic_operations.py file under Mathematics folder

---
+++ Mathematics/arithmetic_operations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5, 6, 7])
+
+# addition:
+print(a + 2)    # output: [3 4 5 6 7 8 9]
+
+# subtraction
+print(a - 2)    # output: [-1  0  1  2  3  4  5]
+
+# multiplication
+print(a * 2)    # output: [ 2  4  6  8 10 12 14]
+
+# addition of two array
+b = np.array([4, 5, 6, 7, 8, 9, 10])
+
+print(a + b)    # output: [ 5  7  9 11 13 15 17]
