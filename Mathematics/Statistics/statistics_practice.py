6f24b6a0900ec0b31ba5494c10ef30ea2ca6d4e2
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-05 17:50:45 +0545

    Max and Min value of first and second row using axis=1, on statistics_practice.py file

--- Mathematics/Statistics/statistics_practice.py
+++ Mathematics/Statistics/statistics_practice.py
@@ -12,3 +12,10 @@ print(np.min(stats))    # output: 1
 
 # finding maximum value
 print(np.max(stats))    # output: 6
+
+# finding minimum value of 1 row and 2 row
+print(np.min(stats, axis=1))    # output: [1 4]
+
+# finding maximum value of 1 row and 2nd row
+print(np.max(stats, axis=1))    # output: [3 6]
+
