64d71481eef95b171bc815b9c26b26bd6bc9632e
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-01-06 18:20:00 +0545

    Joined 2-D array on join_array.py file

--- NumPy Arrays/join_array.py
+++ NumPy Arrays/join_array.py
@@ -3,6 +3,8 @@ import numpy as np
 """
 Join Array: joining means putting contents of two or more arrays in a single array
 """
+# For 1-D array:
+
 # initializing two arrays
 arr1 = np.array([1, 2, 3, 4, 5])
 arr2 = np.array([6, 7, 8, 9, 10])
@@ -10,4 +12,25 @@ arr2 = np.array([6, 7, 8, 9, 10])
 # joining two arrays to make it single array using np.concatenate()
 arr3 = np.concatenate((arr1, arr2))
 
-print(arr3) # output: 
+print(arr3) # output: [ 1  2  3  4  5  6  7  8  9 10]
+
+
+# For 2-D array:
+
+# initializing two arrays
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[6, 7], [8, 9]])
+
+arr3 = np.concatenate((arr1, arr2), axis=1)
+print(arr3) 
+# output: 
+    # [[1 2 6 7]
+    #  [3 4 8 9]]
+
+arr3 = np.concatenate((arr1, arr2), axis=0)
+print(arr3) 
+# Output:
+    # [[1 2]
+    #  [3 4]
+    #  [6 7]
+    #  [8 9]]
